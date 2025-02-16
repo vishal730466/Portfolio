dd37fc26033f3059028be8d16e2320b93a601c6c
vishale <vishalingle9876@gmail.comm>
2025-02-16 10:35:33 +0530

    Skill

--- src/app/project.js
+++ src/app/project.js
@@ -25,18 +25,19 @@ const Project = () => {
       <div className='p1_description'>
           <div> IOT project  </div>
           <div className='gallery'>
-          <img src='car.jpg' height="50%" width="50%"></img>
-          <img src='car.jpg' height="50%" width="50%"></img>
-          <img src='car.jpg' height="50%" width="50%"></img>
-          <img src='car.jpg' height="50%" width="50%"></img>
+          <img src='car.jpg' height="48%" width="45%"></img>
+          <img src='car.jpg' height="48%" width="45%"></img>
+          <img src='car.jpg' height="48%" width="45%"></img>
+          <img src='car.jpg' height="48%" width="45%"></img>
           </div>
+              A remote-controlled car that can be operated via a mobile device.
       </div>
     </div>
   )
   } else {
         return(<div className='mob_project1'>
                  
-         <iframe src="https://tic-tac-toe-lime-two.vercel.app/" height="45%" width="100%" ></iframe>
+         <iframe src="https://tic-tac-toe-lime-two.vercel.app/" height="35%" width="100%" ></iframe>
          <p className='game_dis'>A classic two-player Tic-Tac-Toe game built with Next.js and hosted on Vercel. </p>
      
 
--- src/app/skill.js
+++ src/app/skill.js
@@ -1,20 +1,40 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 const Skill = () => {
     const [val , setval]=useState(2)
+    const [width,setwidth]=useState(1000)
+    useEffect(()=>{
+    if(window){
+     
+      setwidth(window.innerWidth)
+    }
+  },[width])
+
     const reload =()=>{
         setval(val+1)
     }
-  return (
-    <div className='project'>
-        <div className='skill'>
-        <iframe key={val} src='https://3d-web-gilt.vercel.app/' height="90%" width="100%"></iframe>
-        <button onClick={reload}>Reload</button>
-        </div>
 
-    </div>
-  )
+    if (width >450) {
+      return (
+        <div className='project'>
+            <div className='skill'>
+            <iframe key={val} src='https://3d-web-gilt.vercel.app/' height="90%" width="100%"></iframe>
+            <button onClick={reload}>Reload</button>
+            </div>
+    
+        </div>
+      )    
+    } else {
+      return<div className='mob_skill'>
+          {/* <div> */}
+          <iframe key={val} src='https://3d-web-gilt.vercel.app/' height="70%" width="100%" ></iframe>
+            <button onClick={reload}>Reload</button>
+          {/* </div> */}
+
+      </div>
+    }
+  
 }
 
 export default Skill
