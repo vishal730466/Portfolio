fa71225b3ba2311e8f53cdfe2722420de8770223
vishale <vishalingle9876@gmail.comm>
2025-03-07 13:26:12 +0530

    1

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Homepage from "@/app/home";
 import Project from "./project";
 import Project2 from "./project2";
-import Skill from "./skill";
 import Contact from "./Contact/page";
 import Search from "./Threed/page";
 
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <div >
        <Homepage/>
-       {/* <Skill/> */}
       <Search/>
        <Project/>
        <Contact/>
