0a725858b9f417819d96a61d8008baddfd617c66
vishale <vishalingle9876@gmail.comm>
2025-04-02 10:27:23 +0530

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head><Script  async src="https://www.googletagmanager.com/gtag/js?id=G-7CMHBJ3RET"/><Script
+      <head><Script  async src="https://www.googletagmanager.com/gtag/js?id=G-SMJWQN720D"/><Script
           id="google-analytics"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
